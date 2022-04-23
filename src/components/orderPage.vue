5076bb85229fab0112a8ee9ad783123330c64a86
Seiju Nomura <seiju.nomura@gmail.com>
2022-04-23 13:49:40 +0900

    [add] ボタン部分のコンポーネント追加

--- src/components/orderPage.vue
+++ src/components/orderPage.vue
@@ -2,6 +2,7 @@
   <div class="outline">
     <div class="panel">
       left
+      <order-button v-for="product in products" :key="product.name" :product="product" @buy="buy(product)"></order-button>
     </div>
     <div class="panel">
       right
@@ -11,35 +12,55 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import orderButton from './parts/orderButton.vue';
 
 type drink = {
+  id:string;
   name:string;
   price:number;
 }
 
 const coffee : drink = {
+  id:"coffee",
   name:"コーヒー",
   price:480,
 }
 const tea : drink = {
+  id:"tea",
   name:"紅茶",
   price:280,
 }
 const milk : drink = {
+  id:"milk",
   name:"ミルク",
   price:180,
 }
 const coke : drink = {
+  id:"coke",
   name:"コーラ",
   price:190,
 }
 const beer : drink = {
+  id:"beer",
   name:"ビール",
   price:580,
 }
 
+let products : Array<drink> = [coffee, tea, milk, coke, beer];
+
 @Options({
+  components: { orderButton },
   props: {
+  },
+  data(){
+    return {
+      products : products,
+    }
+  },
+  methods:{
+    buy(product : object){
+        console.log(product);
+    }
   }
 })
 export default class orderPage extends Vue {
---
+++ src/components/parts/orderButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="parent">
+    <div class="orderBtn">
+        <input type="button" :id="product.id" @click="buy()"/>
+        <label :for="product.id"><div class="productName">{{product.name}}</div><div class="productPrice">{{product.price}}円</div></label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "orderButton",
+  props:['product'],
+  data() {
+    return {
+        
+    };
+  },
+  methods:{
+    buy(){
+        this.$emit("buy",this.product);
+    }
+  }
+};
+</script>
+
+<style scoped>
+    @import "../../css/orderButton.css";
+</style>
